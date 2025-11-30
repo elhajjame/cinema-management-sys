996fcf106f698e1e83be743a787873815f1aa1fc
mehdi el-hajjame <el-hajjame@outlook.com>
2025-11-30 14:46:16 +0100

    i worked here on creation ticket function, session function and i did some modification in code

--- app.js
+++ app.js
@@ -1,31 +1,88 @@
 const prompt = require('prompt-sync')();
 const spectators = [];
 const sessions = [];
-const ticket = [];
+const tickets = [];
 
-function spectatorsss(){
+let spectatorId = 1;
+let sessionId = 1;
+let ticketId = 1;
+
+
+
+function spectatorsss() {
     const name = prompt("enter Spectator name: ");
-    let objSpectator = {name : name}
+    let objSpectator = { name: name, id: spectatorId++ }
     spectators.push(objSpectator);
-
-   }
-  
-
- 
-function session(){
-
+    console.log("✔ Spectator added!");
 }
 
-// function ticket(){
-//     const objTickets ={
-//         id : ticketId++,
-//         price : price,
 
-//     }
-// }
+
+function session() {
+    const movieName = prompt("enter the movie name: ");
+    const SessionTime = Number(prompt("enter session time:"));
+    const room = Number(prompt("enter room number: "));
+    const category = prompt("enter movie category: ");
+
+    const sessionObj = { sessionId: sessionId++, movieName: movieName, SessionTime: SessionTime, room: room, category: category };
+    sessions.push(session);
+
+    const allowed = ["action", "comedy", "romance", "anime", "drama", "horror"];
+    if (!allowed.includes(category)) {
+        console.log("invalid category");
+    }
+}
+
+function CreateTicket() {
+
+    const spectatorID = prompt("Enter spectator ID: ");
+    const sessionID = prompt("Enter session ID: ");
+
+    let foundSpectator = null;
+    let foundSession = null;
+
+    for (let i = 0; i < spectators.length; i++) {
+        if (spectators[i].id === Number(spectatorID)) {
+            foundSpectator = spectators[i];
+            break;
+        }
+        if (!foundSpectator) {
+            console.log("spectator not found!");
+            return;
+        }
+
+    }
+    for (let i = 0; i < sessions.length; i++) {
+        if (sessions[i].sessionId !== Number(sessionID)) {
+            foundSession = sessions[i];
+            break;
+        }
+        if (!foundSession) {
+            console.log("session not found");
+            return;
+        }
+    }
+    const basePrice = 40;
+    let finalPrice;
+
+    if (foundSession.category === "action")finalPrice = basePrice + 10;
+    if(foundSession.category === "drama") finalPrice += 20;
+    if (foundSession.category === "anime") finalPrice += 15;
+    if(foundSession.category === "comedy") finalPrice += 9;
+    if(foundSession.category === "horror") finalPrice += 19;
+
+    const objTickets = {
+        id: ticketId++,
+        price: finalPrice,
+        category: foundSession.category
+    };
+    tickets.push(objTickets);
+
+        console.log("✔ Ticket created successfully!");
+    console.log(objTickets);
+}
 
 mainMenu();
-
 function mainMenu() {
     while (true) {
         console.log(`
@@ -44,11 +101,20 @@ function mainMenu() {
     ============================
             `);
 
-    const choice = prompt("choose an option: ")
-            switch(choice){
-                case "1":
-                    spectatorsss();
-                    break;
-            }
+        const choice = prompt("choose an option: ")
+        switch (choice) {
+            case "1":
+                spectatorsss();
+                break;
+            case "2":
+                session();
+                break;
+            case "3":
+                CreateTicket();
+                break;
+            case "0":
+                console.log("bye");
+                return;
+        }
     }
 }
